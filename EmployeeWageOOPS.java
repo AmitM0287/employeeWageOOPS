b23f7a1330d2c01ab2502e4771099e7e4c1122c9
AmitM0287 <amitmanna0287@gmail.com>
2020-11-11 17:03:22 +0530

    check employee attendance added!

--- EmployeeWageOOPS.java
+++ EmployeeWageOOPS.java
@@ -1,9 +1,34 @@
+import java.util.Random;
+
 public class EmployeeWageOOPS {
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program!");
 
+		Employee emp = new Employee();
+
+		if (emp.attendance()) {
+			System.out.println("Present");
+
+		} else {
+			System.out.println("Absent");
+		}
 	}
 
+}
+
+class Employee {
+        Random random = new Random();
+
+        public boolean attendance() {
+                int rand = random.nextInt(2);
+
+                if(rand == 1) {
+                        return true;
+
+                } else {
+                        return false;
+                }
+        }
 }
